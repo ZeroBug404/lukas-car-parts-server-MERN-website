e9b2bcf827f8b22ac6b73f8a7fbc94c7759e4259
ZeroBug <404.zerobug@gmail.com>
2022-05-24 01:43:30 +0600

    Initialize lukas server

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const cors = require("cors")
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.np4eg.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run () {
+    try{
+        await client.connect();
+        const productsCollection = client.db('lukas-carParts').collection('products');
+
+        app.get('/products', async(req, res) => {
+            const products = await productsCollection.find({}).toArray();
+            res.send(products)
+        })
+    }
+    catch{
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Lukas on port ${port}`)
+})
