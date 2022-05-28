c5be8264194d87d2e92261745a6c801bd91402c5
ZeroBug <404.zerobug@gmail.com>
2022-05-28 18:06:52 +0600

    updated for cors error

--- README.md
+++ README.md
@@ -2,3 +2,20 @@
 
 ## Live site [Lukar Car Partsd](https://protected-plains-56245.herokuapp.com/).
 ## [https://protected-plains-56245.herokuapp.com/](https://protected-plains-56245.herokuapp.com/).
+
+
+## Technologies
+
+- Site is made with ReactJS
+- Used Firebase for Authentication
+- Used NodeJS as server-side
+- Used ExpressJS as NodeJS
+- Used Mongodb as database
+
+## Features
+
+- Admin has control users
+- Admin has control products like, delete or add new products
+- Admin can make a user admin
+- A user can see his orderd products and manage them
+- Every user and admin can see his or her profile info and update them
--- index.js
+++ index.js
@@ -7,7 +7,18 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const { request } = require("express");
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+const corsConfig = {
+  origin: '*',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+  }
+  app.use(cors(corsConfig))
+  app.options("*", cors(corsConfig))
+  app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,authorization")
+  next()
+  })
 app.use(express.json());
 
 function verifyJWT(req, res, next) {
